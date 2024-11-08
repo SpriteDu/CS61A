2bfd2dd73bb741eebe851d12f022215db9378d42
Jididu <jididu.li@tum.de>
2024-11-08 09:46:16 +0100

    hw05

--- hw/hw05/hw05.py
+++ hw/hw05/hw05.py
@@ -11,6 +11,15 @@ def hailstone(n):
     """
     "*** YOUR CODE HERE ***"
 
+    yield n
+    if n == 1:
+      yield from hailstone(1)
+    elif n%2 == 0 :
+      yield from hailstone(n//2)
+    else:
+      yield from hailstone(n*3+1)
+
+
 
 def merge(a, b):
     """
@@ -30,12 +39,15 @@ def merge(a, b):
     a_val, b_val = next(a), next(b)
     while True:
         if a_val == b_val:
-            "*** YOUR CODE HERE ***"
+            yield a_val
+            a_val, b_val = next(a), next(b)
         elif a_val < b_val:
-            "*** YOUR CODE HERE ***"
-        else:
-            "*** YOUR CODE HERE ***"
+            yield a_val
+            a_val = next(a)
 
+        else:
+            yield b_val
+            b_val = next(b)
 
 def stair_ways(n):
     """
@@ -51,6 +63,16 @@ def stair_ways(n):
     []
     """
     "*** YOUR CODE HERE ***"
+    if n == 0:
+        yield []
+    elif n > 0:
+        for way in stair_ways(n - 1):
+            yield [1] + way
+        for way in stair_ways(n - 2):
+            yield [2] + way
+        
+        
+        
 
 
 def yield_paths(t, value):
@@ -89,12 +111,10 @@ def yield_paths(t, value):
     [[0, 2], [0, 2, 1, 2]]
     """
     if label(t) == value:
-        yield ____
+        yield [label(t)]
     for b in branches(t):
-        for ____ in ____:
-            yield ____
-
-
+        for path in yield_paths(b,value):
+            yield [label(t)] + path
 
 # Tree Data Abstraction
 
