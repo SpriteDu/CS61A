0a96b2ba117ca25986280603979898775b13b2d0
Jididu <jididu.li@tum.de>
2024-11-23 22:02:42 +0100

    h7 finished

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -1,15 +1,23 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+(cond 
+  ((= exp 2) (square base))
+  ((= exp 1) base)
+  ((even? exp) (square(pow base (/ exp 2))))
+  ((odd? exp) (* base (square(pow base (/ (- exp 1) 2)))))
+  )
+)
 
 (define (repeatedly-cube n x)
-  (if (zero? n)
-      x
-      (let (_________________)
-        (* y y y))))
+    (if (zero? n)
+        x
+        (let
+            (( y (pow x (pow 3 (- n 1)))) )
+            (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cdr (cdr s))))
